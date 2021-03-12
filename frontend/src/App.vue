ff4ba3e5864c6369a22e91a58d3510b136546591
Rafaela Ribeiro <rapha11br@gmail.com>
2021-03-12 16:48:13 -0300

    stock-trader

---
+++ frontend/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+	<v-app id="app">
+		<Header/>
+		<Menu/>
+		<Content/>
+	
+		
+	</v-app>
+</template>
+
+<script>
+import Header from './components/Header'
+import Menu from './components/template/Menu'
+import Content from './components/template/Content'
+export default {
+	components: {Header, Menu, Content},
+	created() {
+		this.$store.dispatch('initStocks')
+	}
+
+}
+</script>
+
+<style>
+
+body {
+		margin: 0;
+	}
+	#app {
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+
+		height: 100vh;
+		display: grid;
+		grid-template-rows: 60px 1fr 40px;
+		grid-template-columns: 300px 1fr;
+		grid-template-areas:  
+			'header header'
+			'menu content'
+			
+
+	}
+
+</style>
+
---
+++ frontend/src/components/template/Content.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-content class="content"> 
+			<v-container>
+				<transition name="slide" mode="out-in"> 
+
+				<router-view></router-view>
+				</transition>
+			</v-container>
+		</v-content>
+</template>
+
+<script>
+export default {
+    name:'Content'
+
+}
+</script>
+
+<style>
+ .content {
+        grid-area: content;
+        background: #efefef;
+        padding: 20px;
+        width: 100%
+    }
+
+@keyframes slide-in {
+	from { transform: translateY(-30px); opacity: 0;}
+	to { transform: translateY(0px); opacity: 1;}
+}
+
+@keyframes slide-out {
+	from { transform: translateY(0px); opacity: 1;}
+	to { transform: translateY(-30px); opacity: 0;}
+}
+
+.slide-enter-active{
+	animation: slide-in 0.3s ease;
+
+}
+
+.slide-leave-active{
+	animation: slide-out 0.3s ease;
+	
+}
+</style>
